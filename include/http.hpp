bb130d458f2b80c61b79735d1769a6c2e899f79c
Shin Morisawa <107992426+shinmorisawa@users.noreply.github.com>
2025-05-10 19:24:16 +0900

    Update http.hpp

--- include/http.hpp
+++ include/http.hpp
@@ -27,7 +27,7 @@ namespace HTTP {
 				version = "HTTP/1.1";
 				headers["Content-Length"] = std::to_string(body.size());
 				headers["Content-Type"] = "text/plain";
-				headers["Server"] = "kizuna/0.0.1-release";
+				headers["Server"] = "kizuna/0.0.2-master";
 			}
 
 			std::string toString() const {
